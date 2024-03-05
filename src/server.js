5cc059f0ebec08e2c669856eef1c8b55e9d3d4b9
Natalye <cavalieridev@gmail.com>
2024-03-05 08:15:01 +0100

    create database.db sqlite3

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
+require("express-async-errors")
+const AppError = require("./utils/AppError")
+
+const database = require("./database/sqlite")
+
 const express = require("express")
 
 const routes = require("./routes")
@@ -7,6 +12,22 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+database()
+
+app.use((error, request, response, next) => {
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+  console.error(error)
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 const PORT = 3333
 
