3e772a2bb665f6c4259e45a5fd99aaa73d29afca
Natalye Cavalieri <cavalieridev@gmail.com>
2024-03-25 07:36:18 +0100

    create name and email validation with nulish operator

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -47,8 +47,8 @@ class UsersController {
     if (userIfUpdatedEmail && userIfUpdatedEmail.id !== user.id) {
       throw new AppError("This email already exists");
     }
-    user.name = name;
-    user.email = email;
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
 
     if(password && !old_password){
       throw new AppError("You need insert the new password")
